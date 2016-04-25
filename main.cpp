8afbb4b311659598d4c0ab5402d8541e468e1edf
joshvemula <jyothsnavemula30@gmail.com>
2016-04-25 15:10:44 +0530

    Update main.cpp
    print changed

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
     print_hello();
-    cout<< endl;
-    cout<<"The factorial of 5 is "<< factorial(5)<< endl;
+    
+    printf("The factorial of 5 is %d",factorial(5));
     return 0;
 }
